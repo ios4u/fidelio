7d563db8548e234034ab767b87a11eeadd4fc0dc
Michael Pfister <pfista@gmail.com>
2014-02-08 02:01:08 -0600

    Initial Commit

--- fidelio/ViewController.h
+++ fidelio/ViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CBPeripheralManagerDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *transmitButton;
+@property (weak, nonatomic) IBOutlet UILabel *uuidLabel;
+@property (weak, nonatomic) IBOutlet UILabel *majorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *minorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *identityLabel;
+@property (strong, nonatomic) CLBeaconRegion *beaconRegion; // Define settings for transmitter beacon
+@property (strong, nonatomic) NSDictionary *beaconPeripheralData;
+@property (strong, nonatomic) CBPeripheralManager *peripheralManager; // Control the transmitter
+
+- (IBAction)transmitButton:(id)sender;
 
 @end
--- fidelio/ViewController.m
+++ fidelio/ViewController.m
@@ -12,18 +12,66 @@
 
 @end
 
+bool transmitting = false;
+
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // init beacon
+    [self initBeacon];
+    [self.uuidLabel setText:self.beaconRegion.proximityUUID.UUIDString];
+    [self.majorLabel setText:[NSString stringWithFormat:@"%@", self.beaconRegion.major]];
+    [self.minorLabel setText:[NSString stringWithFormat:@"%@", self.beaconRegion.minor]];
+    [self.identityLabel setText:self.beaconRegion.identifier];
+    
+    // update the labels
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initBeacon {
+    // TODO: make this work for any mac seamlessly
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"3B5B69EC-0D4A-4B0D-8A9C-D9073B0CB062"]; // generated with uuidgen
+    
+    // TODO don't hardcode the major minor etc?
+    self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                                major:1
+                                                                minor:1
+                                                           identifier:@"io.pfista.fidelio"];
+    
+}
+
+-(void) peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn) {
+        NSLog(@"Powered on ");
+        transmitting = true;
+        [self.peripheralManager startAdvertising:self.beaconPeripheralData];
+        [self.transmitButton setTitle:@"Stop Transmission" forState:UIControlStateNormal];
+        
+    } else if (peripheral.state == CBPeripheralManagerStatePoweredOff) {
+        // TODO: ask user to turn bluetooth on
+        NSLog(@"Powered Off");
+        [self.peripheralManager stopAdvertising];
+        [self.transmitButton setTitle:@"Transmit unlock request" forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)transmitButton:(id)sender {
+    if (transmitting) {
+        NSLog(@"Button pressed");
+        self.beaconPeripheralData = [self.beaconRegion peripheralDataWithMeasuredPower:nil];
+        self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:@{CBCentralManagerOptionShowPowerAlertKey:[NSNumber numberWithBool:YES]}];
+
+        [self.transmitButton setTitle:@"Stop Transmission" forState:UIControlStateNormal];
+    }
 }
 
 @end
