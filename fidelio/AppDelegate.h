a67c0f351e77362d6b096a3d6d60d55b605602b6
Michael Pfister <pfista@gmail.com>
2014-02-08 00:44:16 -0600

    Initial Commit

---
+++ fidelio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fidelio
+//
+//  Created by Michael Pfister on 2/8/14.
+//  Copyright (c) 2014 Michael Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fidelio/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fidelio
+//
+//  Created by Michael Pfister on 2/8/14.
+//  Copyright (c) 2014 Michael Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
